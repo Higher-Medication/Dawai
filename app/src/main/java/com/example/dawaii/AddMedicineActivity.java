5a5f73694349d0c85613f06f90dbe9f11170da2c
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-09-27 02:31:24 +0300

    Add-MedicineActivity 2

--- app/src/main/java/com/example/dawaii/AddMedicineActivity.java
+++ app/src/main/java/com/example/dawaii/AddMedicineActivity.java
@@ -20,8 +20,10 @@ public class AddMedicineActivity extends AppCompatActivity {
     private static final String TAG="AddMedicineActivity";
     private TextView displayStartDate;
     private TextView displayEndDate;
+    private TextView expirationDate;
     private DatePickerDialog.OnDateSetListener onDateSetListener;
     private DatePickerDialog.OnDateSetListener onDateSetListener2;
+    private DatePickerDialog.OnDateSetListener onDateSetListener3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -81,6 +83,31 @@ public class AddMedicineActivity extends AppCompatActivity {
                 displayEndDate.setText(date1);
             }
         };
+
+        expirationDate=findViewById(R.id.expirationDate);
+
+        expirationDate.setOnClickListener(view -> {
+            Calendar cal=Calendar.getInstance();
+            int year=cal.get(Calendar.YEAR);
+            int month=cal.get(Calendar.MONTH);
+            int day=cal.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog dialog1=new DatePickerDialog(this,android.R.style.Theme_Holo_Light_Dialog,onDateSetListener3,year,month,day);
+            dialog1.getDatePicker().setMinDate(System.currentTimeMillis() - 1000);
+            dialog1.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog1.show();
+
+        });
+
+        onDateSetListener3= new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                Log.d(TAG, "onDateSet: "+year + "/" + month+ '/' +day+"  ----ExpirationDate");
+                String date1=month+1 + "/" + day+ "/" +year;
+                expirationDate.setText(date1);
+            }
+        };
+
 
 
     }
